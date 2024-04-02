e35f9a5156d1bc80c6263b761a1a28871ccef484
Mario Contreras <mariocc29@gmail.com>
2024-04-02 19:30:10 -0400

    implementing versioning pattern on the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'application#forbidden_exception', via: :all
   
-  get '/movies', to: 'movies#index', defaults: { format: 'json' }
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      get '/movies', to: 'movies#index', defaults: { format: 'json' }
+    end
+  end
 
+  match '*path', to: 'application#route_not_found', via: :al
 end
--- spec/routing/movies_spec.rb
+++ spec/routing/movies_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe MoviesController, type: :routing do
+RSpec.describe Api::V1::MoviesController, type: :routing do
 
   describe('routing') do
     it "routes to #index" do
-      expect(get: "/movies").to route_to("movies#index", format: "json")
+      expect(get: "/api/v1/movies").to route_to("api/v1/movies#index", format: "json")
     end
   end
 
